f78a2e7a8b86a0840dd7575d3ac14c6e5682f83e
jesse-michael-han <jmh288@pitt.edu>
2018-10-25 02:15:30 -0400

    Finished PA induction schema (modulo subst_below code and refactorization thereof)
    Cleaned up peano.lean

--- peano.lean
+++ peano.lean
@@ -7,6 +7,145 @@ local attribute [instance, priority 0] classical.prop_decidable
 namespace peano
 section
 
+ 
+def quantifier_count_preterm {L : Language} : Π n, preterm L n → ℕ := λ n, λ t, 0
+
+
+def quantifier_count_preformula {L : Language} : Π n, preformula L n → ℕ
+| 0 falsum := 0
+| 0 (equal t1 t2) := quantifier_count_preterm _ t1 + quantifier_count_preterm _ t2
+| n (rel R) := 0
+| n (apprel t1 t2) := quantifier_count_preformula _ t1 + quantifier_count_preterm _ t2
+| 0 (f1 ⟹ f2) := quantifier_count_preformula _ f1 + quantifier_count_preformula _ f2
+| 0 (∀' f) := quantifier_count_preformula _ f + 1
+
+--sanity check
+
+#reduce quantifier_count_preformula 0 p_times_succ.fst
+
+#reduce quantifier_count_preformula 0 p_zero_of_times_zero.fst  -- ok
+
+/-- Given a nat k and a 0-formula ψ, return ψ with ∀' applied k times to it --/ 
+@[simp]def alls {L : Language}  :  Π n : ℕ, formula L → formula L
+--:= nat.iterate all n
+| 0 f := f
+| (n+1) f := ∀' alls n f
+
+@[simp] lemma alls_0 {L : Language} (ψ : formula L) : alls 0 ψ = ψ := by refl
+
+@[simp] def b_alls_1 {L : Language} {n : ℕ} {f : formula L} (hf :  formula_below (n+1) f)  : formula_below n $ alls 1 f := begin
+constructor, assumption
+end
+
+@[simp] lemma alls_all_commute {L : Language} (f : formula L) {k : ℕ} : (alls k ∀' f) = (∀' alls k f) :=
+begin
+induction k with k ih,
+simp*,
+simp* -- is this idiomatic?
+end
+
+@[simp] lemma alls_succ_k {L : Language} (f : formula L) {k : ℕ} : alls (k + 1) f = ∀' alls k f := by constructor
+
+@[simp] def b_alls_k {L : Language} {n : ℕ} {k: ℕ} :  ∀ f : formula L, formula_below (n + k) f → formula_below n (alls k f) :=
+begin
+-- induction n with n ih, induction k with k ih,
+induction k with k ih,
+intros f hf, exact hf,
+intros f hf, 
+have H := alls_succ_k,
+
+have hf_rw : formula_below (n + nat.succ k) f → formula_below (n+k) ∀'f, by {apply b_alls_1},
+let hf2 := hf_rw hf,
+
+have hooray := ih (∀'f) hf2,
+rw[alls_all_commute, <-H] at hooray, exact hooray --hooray!!
+end
+
+
+lemma term_below_coe {L : Language} : ∀ {l}, Π n m : ℕ, (n ≤ m) → Π t :(preterm L l), (@term_below L n l t) → (@term_below L m l t)
+  | _ n m _ &k _       := term_below.b_var k begin dedup, cases _x_1, fapply lt_of_lt_of_le, exact n, repeat{assumption}  end
+  | _ n m _ (func f) _                := term_below.b_func f
+  | l n m _ (app t1 t2) _  :=  term_below.b_app t1 t2 
+                               begin
+                               fapply term_below_coe, exact n, assumption, cases _x, assumption
+                               end
+
+                               begin
+                               fapply term_below_coe, exact n, assumption, cases _x, assumption
+                               end
+
+lemma formula_below_coe {L : Language} : ∀ {l}, Π n m : ℕ, (n ≤ m) → Π f : preformula L l, formula_below n f → formula_below m f
+| _ n m _ falsum h := b_falsum
+| _ n m _ (t1 ≃ t2) h := b_equal t1 t2
+                                  begin 
+                                  cases h, fapply term_below_coe, exact n,
+                                  repeat{assumption},
+                                  end
+
+                                  begin 
+                                  cases h, fapply term_below_coe, exact n,
+                                  repeat{assumption},
+                                  end
+
+| _ n m _ (rel R) h := b_rel R
+| l n m _ (apprel f1 f2) h := b_apprel f1 f2 
+                                  begin 
+                                  cases h, fapply formula_below_coe, exact n,
+                                  swap, repeat{assumption}
+                                  end
+
+                                  begin 
+                                  cases h, fapply term_below_coe, exact n,
+                                  swap, repeat{assumption}
+                                  end
+
+| _ n m _ (f1 ⟹ f2) h := b_imp f1 f2
+                                  begin 
+                                  cases h, fapply formula_below_coe, exact n,
+                                  swap, repeat{assumption}
+                                  end
+
+                                  begin 
+                                  cases h, fapply formula_below_coe, exact n,
+                                  swap, repeat{assumption}
+                                  end
+
+| _ n m _ (∀' f) h := b_all f
+                                  begin 
+                                  cases h, fapply formula_below_coe, exact n+1,
+                                  swap, repeat{assumption}, simp*
+                                  end
+
+
+
+/- both b_subst and b_subst2 are defined, but i will refactor later -/
+def b_subst {L : Language} {n : ℕ} {f : formula L} (hf : formula_below (n+1) f) {t : term L} (ht : term_below 0 t) : formula_below n (f[t //0]) :=
+begin
+cases hf,
+constructor,
+constructor,
+cases hf_t₁,
+simp[subst_term],
+simp[subst_realize],
+by_cases hf_t₁_1 < 0,
+exfalso, fapply nat.not_lt_zero, exact hf_t₁_1, assumption,
+simp*, cases hf_t₁_1, simp*, refine term_below_coe t 0 n begin simp* end ht, have hzerolt : 0 < nat.succ hf_t₁_1, by apply nat.zero_lt_succ, simp*, simp* at hf_ht₁, cases hf_ht₁,
+have : hf_t₁_1 < n, sorry -- yuck, need to reorganize
+end
+
+def b_subst2 {L : Language} {n : ℕ} {f : formula L} (hf : formula_below n f) {t : term L} (ht : term_below n t) : formula_below n (f[t //0]) := sorry
+
+
+/- The induction schema instance at ψ is the following formula (up to the fixed ordering of variables given by the de Bruijn indexing):
+
+ - let k be the number of free vars of k,
+
+return (k - 1 ∀∀s)[(ψ(...,0) ∧ (k ∀∀s) (ψ → ψ(...,S(x)))) → (k ∀∀s) ψ]
+-/
+
+/- END LEMMAS -/
+
+/- The language of PA -/
 inductive peano_f0
 | zero
 | one
@@ -102,155 +241,6 @@ split, swap,
 repeat{constructor}
 end
 
-/- Induction schema -/
-
---TODO: finish this
-
--- def var_list_preterm {L : Language} : Π n : ℕ, preterm L n → list ℕ
--- | 0 (var k) := [k]
--- | n (func f) := list.nil
--- | n (app t1 t2) := var_list_preterm _ t1 ∪ var_list_preterm _ t2
-
--- def var_list_preformula {L : Language} : Π n : ℕ, preformula L n → list ℕ
--- | 0 falsum := list.nil
--- | 0 (equal t1 t2) := var_list_preterm _ t1 ∪ var_list_preterm _ t2
--- | n (rel R) := list.nil
--- | n (apprel t1 t2) := var_list_preformula _ t1 ∪ var_list_preterm _ t2
--- | 0 (f1 ⟹ f2) := var_list_preformula _ f1 ∪ var_list_preformula _ f2
--- | 0 (∀' f) := var_list_preformula _ f
- 
-def quantifier_count_preterm {L : Language} : Π n, preterm L n → ℕ := λ n, λ t, 0
-
-
-def quantifier_count_preformula {L : Language} : Π n, preformula L n → ℕ
-| 0 falsum := 0
-| 0 (equal t1 t2) := quantifier_count_preterm _ t1 + quantifier_count_preterm _ t2
-| n (rel R) := 0
-| n (apprel t1 t2) := quantifier_count_preformula _ t1 + quantifier_count_preterm _ t2
-| 0 (f1 ⟹ f2) := quantifier_count_preformula _ f1 + quantifier_count_preformula _ f2
-| 0 (∀' f) := quantifier_count_preformula _ f + 1
-
-
-def free_var_count {L : Language} (n : ℕ) : preformula L n → ℕ :=
-λ ψ, max 0 $ (var_list_preformula n ψ).length - quantifier_count_preformula n ψ
-
---sanity check
-
-#reduce quantifier_count_preformula 0 p_times_succ.fst
-
-#reduce quantifier_count_preformula 0 p_zero_of_times_zero.fst  -- awesome
-
-/-- Given a nat k and a 0-formula ψ, return ψ with ∀' applied k times to it --/ 
-@[simp]def alls {L : Language}  :  Π n : ℕ, formula L → formula L
---:= nat.iterate all n
-| 0 f := f
-| (n+1) f := ∀' alls n f
-
-/-- Return the highest variable in ψ --/
-def highest_variable {L : Language} : Π n, preformula L n → ℕ :=
-begin
-intros n ψ,
-exact list.foldr max 0 (var_list_preformula n ψ)
-end
-
-@[simp] lemma alls_0 {L : Language} (ψ : formula L) : alls 0 ψ = ψ := by refl
-
-@[simp] def b_alls_1 {L : Language} {n : ℕ} {f : formula L} (hf :  formula_below (n+1) f)  : formula_below n $ alls 1 f := begin
-constructor, assumption
-end
-
-@[simp] lemma alls_all_commute {L : Language} (f : formula L) {k : ℕ} : (alls k ∀' f) = (∀' alls k f) :=
-begin
-induction k with k ih,
-simp*,
-simp* -- is this idiomatic?
-end
-
-@[simp] lemma alls_succ_k {L : Language} (f : formula L) {k : ℕ} : alls (k + 1) f = ∀' alls k f := by constructor
-
-@[simp] def b_alls_k {L : Language} {n : ℕ} {k: ℕ} :  ∀ f : formula L, formula_below (n + k) f → formula_below n (alls k f) :=
-begin
--- induction n with n ih, induction k with k ih,
-induction k with k ih,
-intros f hf, exact hf,
-intros f hf, 
-have H := alls_succ_k,
-
-have hf_rw : formula_below (n + nat.succ k) f → formula_below (n+k) ∀'f, by {apply b_alls_1},
-let hf2 := hf_rw hf,
-
-have hooray := ih (∀'f) hf2,
-rw[alls_all_commute, <-H] at hooray, exact hooray --hooray!!
-end
-
-
-set_option trace.app_builder true
-
- -- first step we want is to write a lemma saying we can prove formula_below 0 alls m_1
-
-lemma app_lift_term_below_right {L : Language} : ∀ {l}, Π n m : ℕ, (n ≤ m) → Π t1 : preterm L (l+1), Π t2 : preterm L 0, @term_below L n l (app t1 t2) → @term_below L m 0 t2 :=
-begin
-sorry
-end
-
-lemma term_below_coe {L : Language} : ∀ {l}, Π n m : ℕ, (n ≤ m) → Π t :(preterm L l), (@term_below L n l t) → (@term_below L m l t)
-  | _ n m _ &k _       := term_below.b_var k begin dedup, cases _x_1, finish end
-  | _ n m _ (func f) _                := term_below.b_func f
-  | _ n m _ (app t1 t2) _  :=         begin
-                                      dedup, cases t2, cases t1, repeat{constructor},
-                                      cases _x_2, cases ht₂, finish,
-                                      cases _x_2, cases ht₁,  sorry
-                                      end
-
-lemma formula_below_coe {L : Language} (f : formula L) {n m : nat} (h_le : n ≤ m) (h : formula_below n f) : formula_below m f := sorry
-
-def b_subst {L : Language} {n : ℕ} {f : formula L} (hf : formula_below (n+1) f) {t : term L} (ht : term_below 0 t) : formula_below n (f[t //0]) :=
-begin
-cases hf,
-constructor,
-constructor,
-cases hf_t₁,
-simp[subst_term],
-simp[subst_realize],
-by_cases hf_t₁_1 < 0,
-exfalso, fapply nat.not_lt_zero, exact hf_t₁_1, assumption,
-simp*, cases hf_t₁_1, simp*, refine term_below_coe t 0 n begin simp* end ht, have hzerolt : 0 < nat.succ hf_t₁_1, by apply nat.zero_lt_succ, simp*, simp* at hf_ht₁, cases hf_ht₁,
-have : hf_t₁_1 < n, sorry -- yuck, need to reorganize
-end
-
-/- Note: this has already been proved, see term_below_subst -/
-def b_subst2 {L : Language} {n : ℕ} {f : formula L} (hf : formula_below n f) {t : term L} (ht : term_below n t) : formula_below n (f[t //0]) := sorry
-
-@[simp]lemma not_zero_pm_1 (n : ℕ) (h : n ≠ 0) : (n - 1) + 1 = n :=
-begin
-cases n,
-tidy
-end
-
-
-/- The induction schema instance at ψ is the following formula (up to the fixed ordering of variables given by the de Bruijn indexing):
-
- - let k be the number of free vars of k,
-
-return (k - 1 ∀∀s)[(ψ(...,0) ∧ (k ∀∀s) (ψ → ψ(...,S(x)))) → (k ∀∀s) ψ]
--/
-
-/- Given a unary function F, apply it at &n inside the formula f. This is a substitute for dependent substitution, which isn't implemented yet.  -/ 
-def apply_at_variable_term {L : Language} : Π(F : L.functions 1), ∀{l}, preterm L l → ℕ → preterm L l
-| F _ &k            n := subst_realize (λ x, begin fapply app, apply func, exact F, exact &x end) (fol.lift_term &n k) n k
-| F _ (func f)      n := func f
-| F _ (app t1 t2)   n := app (apply_at_variable_term F t1 n) (apply_at_variable_term F t2 n)
-
-def apply_at_variable {L : Language} : Π (F : L.functions 1), ∀ {l},(preformula L l) → ℕ → preformula L l
-| F _ falsum           n := falsum
-| F _ (t1 ≃ t2)         n := apply_at_variable_term F t1 n ≃ apply_at_variable_term F t2 n
-| F _ (rel R)          n :=  rel R
-| F _ (apprel f t)      n := apprel (apply_at_variable F f n) (apply_at_variable_term F t n)
-| F _ (f1 ⟹ f2)          n := apply_at_variable F f1 n ⟹ apply_at_variable F f2 n
-| F _ (∀' f)            n := ∀' apply_at_variable F f n
-
-def b_apply_at_variable {L : Language} {f : formula L} {F} {n} (h : formula_below n f) : (formula_below n $ apply_at_variable F f 0) := sorry
-
 def p_induction_schema (n : ℕ) (ψ : formula L_peano) (hψ : formula_below n ψ)  : @sentence L_peano := -- add a hypothesis that ψ is in formula_below k and then do k_foralls
 begin
 --  let k := free_var_count 0 ψ,
@@ -266,13 +256,15 @@ split,swap,
   begin
   simp, apply b_subst, by_cases n = 0, simp*,
   rw[h] at hψ, fapply formula_below_coe, exact 0, simpa,
-  simp*, assumption,
+  simp*, have : (1 + (n - 1)) = n,
+  cases n, exfalso, finish, simp*, rw[this], assumption,
   constructor
   end,
 
   begin 
   by_cases n = 0, simp*,
-  rw[h] at hψ, fapply formula_below_coe, exact 0, simpa, simp*, assumption,
+  rw[h] at hψ, fapply formula_below_coe, exact 0, simpa, simp*, have : (1 + (n - 1)) = n,
+  cases n, exfalso, finish, simp*, rw[this], assumption,
   end,
 
   begin
