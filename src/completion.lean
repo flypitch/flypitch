03143ac97c0e64b517b5ceae916f6f7d0742cd6f
flypitch <jmh288@pitt.edu>
2018-11-06 01:05:23 -0500

    Finish completion.lean

--- src/completion.lean
+++ src/completion.lean
@@ -624,46 +624,21 @@ have witness_property := witness.property, cases witness_property with case1 cas
 exact case2,
 end
 
-@[reducible]def list_is_list_of_subtype (L : Language) (fs : list (sentence L))
-: Σ' xs : list ↥{f : sentence L | f ∈ fs}, ∀ f, ∀ h : f ∈ fs, (⟨f,h⟩ : ↥{f : sentence L | f ∈ fs}) ∈ xs
-
-
--- begin
---   induction fs, split, swap,
---   exact [], simp,
-
---   split, swap, refine _::_,
---   split, swap, exact fs_hd, simp,
---   have : {f | f ∈ fs_tl} → {f | f ∈ list.cons fs_hd fs_tl},
---   intro f, split, swap, exact f.val, exact or.inr f.property,
-  
---   induction fs_tl,
---     exact [],
---     refine _::_,
---     refine ⟨fs_tl_hd, _⟩, finish,
-    
-    
--- --  fapply list.map this, exact fs_ih.fst,
-  
---   intros f hf, cases hf, fapply or.inl, simp*, fapply or.inr,
---   have := fs_ih.snd f hf, have hf2 : f ∈ fs_tl, exact hf,
-  
---   have : list.mem (⟨f, _⟩ : ↥{f : sentence L | f ∈ fs_tl}) (list.map (λ (f : ↥{f : sentence L | f ∈ fs_tl}), ⟨f.val, _⟩) (fs_ih.fst)) = ((⟨f, _⟩ : ↥{f : sentence L | f ∈ fs_tl}) ∈ (list.map (λ (f : ↥{f : sentence L | f ∈ fs_tl}), (⟨f.val, _⟩ : ↥{f : sentence L | f ∈ fs_tl})) (fs_ih.fst))),
---   simp*, fapply propext, split, intro, exact this, intro, unfold has_mem.mem at this, repeat{sorry},
-  
---   -- have : list.mem (⟨f, _⟩: ↥{f : sentence L | f ∈ fs_tl})) (list.map (λ (f : ↥{f : sentence L | f ∈ fs_tl}), (⟨f.val, _⟩ : ↥{f : sentence L | f ∈ fs_tl}))) (fs_ih.fst)) = ⟨f, _⟩ ∈ (list.map (λ (f : ↥{f : sentence L | f ∈ fs_tl}), ⟨f.val, _⟩) (fs_ih.fst)),
-
-
---   -- fapply @list.mem_map_of_mem ↥{f : sentence L | f ∈ fs_tl} ↥{f : sentence L | f ∈ fs_tl} (λ (f : ↥{f : sentence L | f ∈ fs_tl}),
--- --   have H := @list.mem_map_of_mem  ↥{f : sentence L | f ∈ fs_tl}  ↥{f : sentence L | f ∈ fs_tl} (λ (f : ↥{f : sentence L | f ∈ fs_tl}), ((subtype.mk f.val _) : ↥{f : sentence L | f ∈ fs_tl})) ⟨f,_⟩ fs_ih.fst this, unfold has_mem.mem at H,
--- --   have : (⟨f, hf⟩ : ↥{f : sentence L | f ∈ fs_tl}).val = f, by refl, simp[*, -H] at H, assumption,
-  
--- -- begin dedup, exact f_1.property end,
-
--- --fapply list.mem_map_of_mem,
-
-
--- end
+def list_is_list_of_subtype : Π(L : Language), Π (fs : list (sentence L)),  Σ' xs : list ↥{f : sentence L | f ∈ fs}, ∀ f, ∀ h : f ∈ fs, (⟨f,h⟩ : ↥{f : sentence L | f ∈ fs}) ∈ xs
+| L [] := begin simp*,  split, exact [], trivial end
+| L (list.cons hd tl) :=
+  begin
+    split, swap,
+    have ih := list_is_list_of_subtype L tl,
+    have F : {f | f ∈ tl} → {f | f ∈ list.cons hd tl},
+    intro f, refine ⟨f, _⟩, fapply or.inr, exact f.property,
+    have ih_image := list.map F ih.fst,
+    refine _::(ih_image),
+    split, swap, exact hd, exact or.inl rfl,
+    intros f hf, simp*,
+    cases hf, exact or.inl hf,
+    fapply or.inr, fapply exists.intro, exact hf, exact (list_is_list_of_subtype L tl).snd f hf
+  end
 
 noncomputable def finite_sup_T_lemma_2 {L : Language} (T : Theory L) (hT : is_consistent T) (Ts : set (Theory_over T hT)) (hTs : nonempty Ts) (h_chain : chain Theory_over_subset Ts) (h_inconsis : T ∪ ⋃₀(subtype.val '' Ts) ⊢' (⊥ : sentence L)) (fs : list (sentence L)) (Hfs : {ϕ : sentence L | ϕ ∈ fs} ⊢' (⊥ : sentence L) ∧ {ϕ : sentence L | ϕ ∈ fs} ⊆ T ∪ ⋃₀(subtype.val '' Ts)) (dSs : Π (f : sentence L), f ∈ fs → (Σ' (S_f : Theory_over T hT), set.mem S_f Ts ∧ set.mem f (S_f.val))) :  Σ' (T_max : Theory_over T hT), T_max ∈ Ts ∧ ∀ (ψ : sentence L), ψ ∈ fs → ψ ∈ T_max.val :=
 begin
