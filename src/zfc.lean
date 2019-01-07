477bb89c8c14b99cb9c9f90884762433256ecefd
andrew <ajtindall63@gmail.com>
2019-01-07 00:43:20 -0500

    Refactoring ZFC Prefix --> infix, and up-arrows --> explicit casting. ALso renamed ordinal lemmas

--- src/zfc.lean
+++ src/zfc.lean
@@ -44,8 +44,9 @@ def str_formula : ∀ {n : ℕ}, bounded_formula L_ZFC n → ℕ → string
   | n (bd_apprel (f₁) f₂) m := str_preformula n 1 m f₁ ++ "(" ++ str_term n m f₂ ++ ")"
   | n (bd_imp a b) m := "(" ++ str_formula a m ++ " ⟹ " ++ str_formula b m ++ ")"
 
-
 def print_formula : ∀ {n : ℕ}, bounded_formula L_ZFC n → string := λ n f, str_formula f n
+
+notation `lift_cast` := by {repeat{apply nat.succ_le_succ}, apply nat.zero_le}
 
 -- section test
 
@@ -66,20 +67,20 @@ def functional {n} (c : bounded_formula L_ZFC (n+2)) : bounded_formula L_ZFC n :
 ∀' ∃' ∀' (c ↑' 1 # 1 ⇔ &0 ≃ &1)
 def subset : bounded_formula L_ZFC 2 := ∀' (&0 ∈' &1 ⟹ &0 ∈' &2)
 def is_emptyset : bounded_formula L_ZFC 1 := ∼ ∃' (&0 ∈' &1) 
-def pair : bounded_formula L_ZFC 3 := bd_equal &0 &1 ⊔ bd_equal &0 &2
+def pair : bounded_formula L_ZFC 3 := (&0 ≃ &1 : bounded_formula L_ZFC 3) ⊔ (&0 ≃ &2 : bounded_formula L_ZFC 3)
 def identity_relation : bounded_formula L_ZFC 2 := &0 ≃ &1
 def singl : bounded_formula L_ZFC 2 := &0 ≃ &1
 def binary_union : bounded_formula L_ZFC 3 := &0 ∈' &1 ⊔ &0 ∈' &2
-def succ : bounded_formula L_ZFC 2 := bd_equal &0 &1 ⊔ &0 ∈' &1 
+def succ : bounded_formula L_ZFC 2 := (&0 ≃ &1 : bounded_formula L_ZFC 2) ⊔ &0 ∈' &1 
 --∀x∃y(x ∈ y ∧ ∀z(z ∈ y → ∃w(z ∈ w ∧ w ∈ y)))
 
 
 
 
-def ordered_pair : bounded_formula L_ZFC 3 := ∀'(&0 ∈' &1 ⇔ (bd_equal &0 &3) ⊔ ∀'(&0 ∈' &1 ⇔ pair ↑' 1 # 1 ↑' 1 # 1))
+def ordered_pair : bounded_formula L_ZFC 3 := ∀'(&0 ∈' &1 ⇔ (&0 ≃ &3 : bounded_formula L_ZFC 4) ⊔ ∀'(&0 ∈' &1 ⇔ pair ↑' 1 # 1 ↑' 1 # 1))
 -- &0 is an ordered pair of &2 and &1 (z = ⟨x, y⟩)
 
-def ordered_pair' : bounded_formula L_ZFC 3 := ∀'(&0 ∈' &3 ⇔ (bd_equal &0 &2) ⊔ ∀'(&0 ∈' &1 ⇔ pair ↑' 1 # 1 ↑' 1 # 4))
+def ordered_pair' : bounded_formula L_ZFC 3 := ∀'(&0 ∈' &3 ⇔ (&0 ≃ &2 : bounded_formula L_ZFC 4) ⊔ ∀'(&0 ∈' &1 ⇔ (pair ↑' 1 # 1).cast(lift_cast)))
 -- &2 is an ordered pair of &1 and &0 (x = ⟨y,z⟩)
 
 def is_ordered_pair : bounded_formula L_ZFC 1 := ∃' ∃' ordered_pair
@@ -88,43 +89,43 @@ def is_ordered_pair : bounded_formula L_ZFC 1 := ∃' ∃' ordered_pair
 def relation : bounded_formula L_ZFC 1 := ∀' ((&0 ∈' &1) ⟹ is_ordered_pair ↑' 1 # 1)
 --&0 is a relation (is a set of ordered pairs)
 
-def function : bounded_formula L_ZFC 1 := relation ⊓ ∀'∀'∀'∀'∀'(&1 ∈' &5 ⊓ ordered_pair ↑' 1 # 3 ↑' 1 # 1 ↑' 1 # 0 ⊓ (&0 ∈' &5 ⊓ ordered_pair ↑' 1 # 3 ↑' 1 # 2 ↑' 1 # 1) ⟹ (bd_equal &3 &2))
+def function : bounded_formula L_ZFC 1 := relation ⊓ ∀'∀'∀'∀'∀'(&1 ∈' &5 ⊓ ordered_pair ↑' 1 # 3 ↑' 1 # 1 ↑' 1 # 0 ⊓ ((&0 ∈' &5 : bounded_formula L_ZFC 6) ⊓ (ordered_pair ↑' 1 # 2 ↑' 1 # 1).cast(lift_cast)) ⟹ (&3 ≃ &2 : bounded_formula L_ZFC 6))
 -- X is a function iff X is a relation and the following holds:
 -- ∀x ∀y ∀z ∀w ∀t, ((w ∈ X) ∧ (w = ⟨x, y⟩) ∧ (z ∈ X) ∧ (z = ⟨x, z⟩ ))) →  y = z
 
 
-def fn_app : bounded_formula L_ZFC 3 := ∃'(&0 ∈' &3 ⊓ ∀'(&0 ∈' &1 ⇔ (bd_equal &0 &3) ⊔ pair ↑' 1 # 1 ↑' 1 # 4))
+def fn_app : bounded_formula L_ZFC 3 := ∃'(&0 ∈' &3 ⊓ ∀'(&0 ∈' &1 ⇔ ((&0 ≃ &3): bounded_formula L_ZFC 5) ⊔ (pair ↑' 1 # 1).cast(lift_cast)))
 -- ⟨&1, &0⟩ ∈ &2 
 -- &0 = &2(&1) 
 
 def fn_domain : bounded_formula L_ZFC 2 := ∀'(&0 ∈' &2 ⇔ ∃'∃'(ordered_pair ↑' 1 # 1 ↑' 1 # 1 ⊓ &0 ∈' &3))
 -- &1 is the domain of &0
 
-def fn_range : bounded_formula L_ZFC 2 := ∀'(&0 ∈' &2 ⇔ ∃'∃'(∀'(&0 ∈' &1 ⇔ (bd_equal &0 &2) ⊔ pair ↑' 1 # 1 ↑' 1 # 4 ↑' 1 # 4) ⊓ &0 ∈' &3))
+def fn_range : bounded_formula L_ZFC 2 := ∀'(&0 ∈' &2 ⇔ ∃'∃'(∀'(&0 ∈' &1 ⇔ (&0 ≃ &2 : bounded_formula L_ZFC 6) ⊔ (pair ↑' 1 # 1).cast(lift_cast)) ⊓ &0 ∈' &3))
 --&1 is the range of &0
 
-def inverse_relation : bounded_formula L_ZFC 2 := ∀'(&0 ∈' &1 ⇔ ∃'∃'(ordered_pair' ↑' 1 # 3 ↑' 1 # 3 ⊓ ∃'(∀'(&0 ∈' &1 ⇔ (bd_equal &0 &2) ⊔ pair ↑' 1 # 3 ↑' 1 # 3 ↑' 1 # 3 ↑' 1 # 1) ⊓ &0 ∈' &5)))
+def inverse_relation : bounded_formula L_ZFC 2 := ∀'(&0 ∈' &1 ⇔ ∃'∃'(∃'(∀'(&0 ∈' &1 ⇔ (&0 ≃ &2 : bounded_formula L_ZFC 7) ⊔ (pair ↑' 1 # 1).cast(lift_cast)) ⊓ &0 ∈' &5 : bounded_formula L_ZFC 6)) ⊓ (ordered_pair'.cast(lift_cast))
 -- &0 is the inverse relation of &1
 
-def function_one_one : bounded_formula L_ZFC 1 := function ⊓ ∀' (inverse_relation ⟹ function ↑' 1 # 1)
+def function_one_one : bounded_formula L_ZFC 1 := function ⊓ ∀' (inverse_relation ⟹ function.cast(lift_cast))
 
-def irreflexive_relation : bounded_formula L_ZFC 2 := relation ↑' 1 # 1 ⊓ ∀'(&0 ∈' &2 ⟹ (∀'(∀'(&0 ∈' &1) ⇔ bd_or (bd_equal &0 &2) ∀'(&0 ∈' &1 ⇔ (bd_equal &0 &3))) ⟹ ∼(&0 ∈' &3)))
+def irreflexive_relation : bounded_formula L_ZFC 2 :=  (∀'(&0 ∈' &2 ⟹ (∀'(∀'(&0 ∈' &1) ⇔ ((&0 ≃ &2 : bounded_formula L_ZFC 4) ⊔ ∀'(&0 ∈' &1 ⇔ (&0 ≃ &3 : bounded_formula L_ZFC 5))) ⟹ ∼(&0 ∈' &3))))) ⊓ relation.cast(lift_cast)
 -- &0 is an irreflexive relation on &1
 
-def transitive_relation : bounded_formula L_ZFC 2 := relation ↑' 1 # 1 ⊓  ∀'∀'∀'((bd_and (bd_and (&2 ∈' &4) (&1 ∈' &4)) (&0 ∈' &4)) ⊓ ∃'(ordered_pair ↑' 1 # 1 ↑' 1 # 4 ↑' 1 # 4 ⊓ &0 ∈' &4) ⊓ ∃'(ordered_pair ↑' 1 # 3 ↑' 1 # 3 ↑' 1 # 3 ⊓ &0 ∈' &4) ⟹ ∃'(ordered_pair ↑' 1 # 2 ↑' 1 # 4 ↑' 1 # 4 ⊓ &0 ∈' &4))
+def transitive_relation : bounded_formula L_ZFC 2 := ((∀'∀'∀'((((&2 ∈' &4 : bounded_formula L_ZFC 5) ⊓ (&1 ∈' &4)) ⊓ (&0 ∈' &4)) ⊓ ∃'((ordered_pair ↑' 1 # 1).cast(lift_cast) ⊓ (&0 ∈' &4 : bounded_formula L_ZFC 6)) ⊓ ∃'(ordered_pair.cast(lift_cast) ⊓ &0 ∈' &4 : bounded_formula L_ZFC 6) ⟹ ∃'((ordered_pair ↑' 1 # 2).cast(lift_cast) ⊓ &0 ∈' &4 : bounded_formula L_ZFC 6)))) ⊓ relation.cast(lift_cast) 
 --&0 is a transitive relation on &1
 -- X Tr Y iff X is a relation and the following holds:
 -- ∀u ∀v ∀w, (u ∈ Y ∧ v ∈ Y ∧ w ∈ Y ∧ ⟨u, v⟩ ∈ X ∧ ⟨v,w⟩ ∈ X) → ⟨u,w⟩ ∈ X 
 
 def partial_order_zfc : bounded_formula L_ZFC 2 := irreflexive_relation ⊓ transitive_relation
 
-def connected_relation : bounded_formula L_ZFC 2 := relation ↑' 1 # 1 ⊓ ∀'∀'((bd_and (bd_and (&0 ∈' &3) (&1 ∈' &3)) ∼(bd_equal &0 &1)) ⟹ ∃'(&0 ∈' &3 ⊓ (ordered_pair ↑' 1 # 3 ↑' 1 # 3 ⊔ ∀'(&0 ∈' &1 ⇔ (bd_equal &0 &2) ⊔ pair ↑' 1 # 1 ↑' 1 # 4 ↑' 1 # 4))))
+def connected_relation : bounded_formula L_ZFC 2 := relation ↑' 1 # 1 ⊓ ∀'∀'((bd_and (bd_and (&0 ∈' &3) (&1 ∈' &3)) ∼(bd_equal &0 &1)) ⟹ ∃'((&0 ∈' &3 : bounded_formula L_ZFC 5) ⊓ ((ordered_pair ↑' 1 # 3 ↑' 1 # 3) ⊔ ∀'(&0 ∈' &1 ⇔ (&0 ≃ &2 : bounded_formula L_ZFC 6) ⊔ (pair ↑' 1 # 1).cast(lift_cast)))))
 --&0 is a connected relation on &1
 -- X Con Y iff Rel(X) and ∀u ∀v (u ∈ Y ∧ v ∈ Y ∧ u ≠ v) → ⟨u,v⟩ ∈ X ∨ ⟨v, u⟩ ∈ X
 
 def total_order : bounded_formula L_ZFC 2 := irreflexive_relation ⊓ transitive_relation ⊓ connected_relation
 
-def well_order : bounded_formula L_ZFC 2 := irreflexive_relation ⊓ ∀'(subset ↑' 1 # 2 ⊓ ∃'(&0 ∈' &1) ⟹ ∃'(&0 ∈' &1 ⊓ ∀'(bd_and (&0 ∈' &2)  ∼(bd_equal &0  &1) ⟹ ∃'(ordered_pair ↑' 1 # 4 ↑' 1 # 4 ↑' 1 # 4 ⊓ &0 ∈' &4) ⊓ ∼∃'(∀'(&0 ∈' &1 ⇔ (bd_equal &0  &2) ⊔ pair ↑' 1 # 1 ↑' 1 # 4 ↑' 1 # 4 ↑' 1 # 4) ⊓ &0 ∈' &4))))
+def well_order : bounded_formula L_ZFC 2 := irreflexive_relation ⊓ ∀'(subset ↑' 1 # 2 ⊓ ∃'(&0 ∈' &1) ⟹ ∃'(&0 ∈' &1 ⊓ ∀'(((&0 ∈' &2) ⊓  ∼(&0 ≃ &1 : bounded_formula L_ZFC 5)) ⟹ ∃'(ordered_pair.cast(lift_cast) ⊓ (&0 ∈' &4 : bounded_formula L_ZFC 6)) ⊓ ∼∃'(∀'(&0 ∈' &1 ⇔ (( &0 ≃ &2 : bounded_formula L_ZFC 7) ⊔ (pair ↑' 1 # 1).cast(lift_cast) )) ⊓ &0 ∈' &4))))
 -- &0 well-orders &1
 
 def membership_relation : bounded_formula L_ZFC 1 := relation ⊓ ∀'(&0 ∈' &1 ⇔ ∃'∃'∀'(&0 ∈' &3 ⇔ ((bd_equal &0  &2) ⊔ pair ↑' 1 # 3 ↑' 1 # 3) ⊓ &2 ∈' &1))
@@ -133,7 +134,7 @@ def membership_relation : bounded_formula L_ZFC 1 := relation ⊓ ∀'(&0 ∈' &
 def transitive_zfc : bounded_formula L_ZFC 1 := ∀' ((&0 ∈' &1) ⟹ subset)
 --&0 is transitive
 
-def fn_zfc_equiv : bounded_formula L_ZFC 3 := ((function_one_one ↑' 1 # 1 ↑' 1 # 1) ⊓ (fn_domain ↑' 1 # 1)) ⊓ (fn_range ↑' 1 # 2)
+def fn_zfc_equiv : bounded_formula L_ZFC 3 := ((function_one_one.cast(lift_cast)) ⊓ (fn_domain ↑' 1 # 1)) ⊓ (fn_range ↑' 1 # 2)
 
 def zfc_equiv : bounded_formula L_ZFC 2 := ∃' fn_zfc_equiv
 --&0 ≃ &1, i.e. they are equinumerous
@@ -141,24 +142,25 @@ def zfc_equiv : bounded_formula L_ZFC 2 := ∃' fn_zfc_equiv
 def is_powerset : bounded_formula L_ZFC 2 := ∀' ((&0 ∈' &2) ⇔ subset ↑' 1 # 2)
 --&1 is P(&0)
 
-def is_suc_of : bounded_formula L_ZFC 2 := ∀' ((&0 ∈' &2) ⇔ ((&0 ∈' &1) ⊔ (bd_equal &0 &1)))
+def is_suc_of : bounded_formula L_ZFC 2 := ∀' ((&0 ∈' &2) ⇔ ((&0 ∈' &1) ⊔ ( &0 ≃ &1 : bounded_formula L_ZFC 3)))
 -- &1 = succ(&0)
 
-def is_ordinal : bounded_formula L_ZFC 1 := (∀' ((membership_relation ↑' 1 # 1) ⟹ well_order)) ⊓ transitive_zfc
+def is_ordinal : bounded_formula L_ZFC 1 := (∀' ((membership_relation.cast(lift_cast)) ⟹ well_order)) ⊓ transitive_zfc
 
 def is_suc_ordinal : bounded_formula L_ZFC 1 := is_ordinal ⊓ ∃' is_suc_of
 --&0 is a successor ordinal
 
-def ordinal_lt : bounded_formula L_ZFC 2 := (is_ordinal ↑' 1 # 1) ⊓ (is_ordinal ↑' 1 # 0) ⊓ (&0 ∈' &1)
+def ordinal_lt : bounded_formula L_ZFC 2 := (is_ordinal.cast(lift_cast)) ⊓ (is_ordinal ↑' 1 # 0) ⊓ (&0 ∈' &1)
 -- &0 < &1
 
 def ordinal_le : bounded_formula L_ZFC 2 := ordinal_lt ⊔ (bd_equal &0 &1)
 -- &0 ≤ &1
+#print is_uncountable_ordinal
 
-def is_first_ordinal : bounded_formula L_ZFC 1 := ∀' ((&0 ∈' &1) ⇔ (((is_emptyset ⊔ is_suc_ordinal)↑' 1 # 1) ⊓ ∀'((&0 ∈' &1) ⟹ ((is_emptyset ⊔ is_suc_ordinal) ↑' 1 # 1 ↑' 1 # 1))))
+def is_first_infinite_ordinal : bounded_formula L_ZFC 1 := ∀' ((&0 ∈' &1) ⇔ (((is_emptyset ⊔ is_suc_ordinal)↑' 1 # 1) ⊓ ∀'((&0 ∈' &1) ⟹ ((is_emptyset ⊔ is_suc_ordinal).cast(lift_cast)))))
 --&0 = ω
 
-def is_uncountable_ordinal : bounded_formula L_ZFC 1 := ∀' ((is_first_ordinal ↑' 1 # 1) ⟹ ∀' (subset ↑' 1 # 2 ⟹(∼(zfc_equiv ↑' 1 # 1))))
+def is_uncountable_ordinal : bounded_formula L_ZFC 1 := ∀' ((is_first_infinite_ordinal.cast(lift_cast)) ⟹ ∀' (subset.cast(lift_cast) ⟹(∼(zfc_equiv ↑' 1 # 1))))
 --&0 ≥ ω₁
 
 def is_first_uncountable_ordinal : bounded_formula L_ZFC 1 := is_uncountable_ordinal ⊓ (∀' ((is_uncountable_ordinal ↑' 1 # 1) ⟹ ordinal_le))
@@ -166,7 +168,7 @@ def is_first_uncountable_ordinal : bounded_formula L_ZFC 1 := is_uncountable_ord
 
 
 
-def continuum_hypothesis : sentence L_ZFC := ∀' ∀'  (( (∃'  ((is_first_ordinal ↑' 1 # 1 ↑' 1 # 1) ⊓(is_powerset↑' 1 # 2))) ⊓ (is_first_uncountable_ordinal ↑' 1 #0)) ⟹ zfc_equiv)
+def continuum_hypothesis : sentence L_ZFC := ∀' ∀'  (( (∃'  ((is_first_infinite_ordinal ↑' 1 # 1 ↑' 1 # 1) ⊓(is_powerset↑' 1 # 2))) ⊓ (is_first_uncountable_ordinal ↑' 1 #0)) ⟹ zfc_equiv)
 
 
 
@@ -194,8 +196,6 @@ def axiom_of_separation (c : Class) : sentence L_ZFC := ∀' (small $ &0 ∈' &1
 def axiom_of_pairing : sentence L_ZFC := ∀' ∀' small pair
 --the class consisting of the ordered pair ⟨x, y⟩
 def axiom_of_ordered_pairing : sentence L_ZFC := ∀' ∀' small ordered_pair
---the class consisting of all ordered pairs
-def axiom_of_product : sentence L_ZFC := small is_ordered_pair
 
 def ZF : Theory L_ZFC := {axiom_of_extensionality, axiom_of_union, axiom_of_powerset, axiom_of_infinity} ∪ (λ(c : bounded_formula L_ZFC 2), axiom_of_replacement c) '' set.univ
 
