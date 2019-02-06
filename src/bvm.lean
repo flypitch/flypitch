00d44f2ca0d7dd900eed685015470890bd2038d6
flypitch <jmh288@pitt.edu>
2019-02-05 20:53:10 -0500

    Finish deduction theorem for boolean algebras

--- src/bvm.lean
+++ src/bvm.lean
@@ -4,15 +4,57 @@ open lattice
 
 universe u
 
-variables (β : Type u) [complete_boolean_algebra β]
+/- A β-valued model of ZFC -/
+
+namespace lattice
+def imp {α : Type*} [boolean_algebra α] : α → α → α :=
+  λ a₁ a₂, (- a₁) ⊔ a₂
+
+infix ` ⟹ `:50 := lattice.imp
+
+lemma imp_neg_sub {α : Type*} [boolean_algebra α] {a₁ a₂ : α} :  -(a₁ ⟹ a₂) = a₁ - a₂ :=
+  by {rw[sub_eq, imp]; finish}
+
+lemma inf_eq_of_le {α : Type*} [distrib_lattice α] {a b : α} (h : a ≤ b) : a ⊓ b = a :=
+  by apply le_antisymm; finish[le_inf]
+
+/-- the deduction theorem in β -/
+lemma imp_top_iff_le {α : Type*} [boolean_algebra α] {a₁ a₂ : α} : (a₁ ⟹ a₂ = ⊤) ↔ a₁ ≤ a₂ :=
+begin
+ split; intro H,
+  {have : a₁ ⊓ a₂ = a₁, from
+    calc a₁ ⊓ a₂ = ⊥ ⊔ (a₁ ⊓ a₂) : by simp
+             ... = (a₁ ⊓ - a₁) ⊔ (a₁ ⊓ a₂) : by simp
+             ... = a₁ ⊓ (- a₁ ⊔ a₂) : by {rw[inf_sup_left]}
+             ... = a₁ ⊓ ⊤ : by {rw[<-H], refl}
+             ... = a₁ : by {simp},
+             
+   finish},
+ {have : a₁ ⊓ a₂ = a₁, from inf_eq_of_le H, apply top_unique,
+  have this' : ⊤ = - a₁ ⊔ a₁, by simp, rw[this', <-this, imp], simp, repeat{split},
+  suffices : (- a₁ ≤ - a₁ ⊔ - a₂ ⊔ a₂) = (- a₁ ≤ - a₁ ⊔ (- a₂ ⊔ a₂)),
+    by rw[this]; apply le_sup_left, ac_refl,
+  suffices : (-a₂ ≤ -a₁ ⊔ -a₂ ⊔ a₂) = (- a₂ ≤ - a₂ ⊔ (-a₁ ⊔ a₂)),
+    by rw[this]; apply le_sup_left, ac_refl,
+  suffices : - a₁ ⊔ - a₂ ⊔ a₂ = ⊤,
+    by rw[this]; simp, apply top_unique,
+    suffices : - a₁ ⊔ - a₂ ⊔ a₂ = - a₁ ⊔ (- a₂ ⊔ a₂),
+      by simp[this], ac_refl}
+end
+end lattice
+
+
+namespace bSet
 
 lemma forall_and {α : Type*} {p q : α → Prop} : (∀ x, p x) ∧ (∀ y, q y) ↔ ∀ x, p x ∧ q x :=
   by finish
 
 -- τ is a B-name if and only if τ is a set of pairs of the form ⟨σ, b⟩, where σ is
 -- a B-name and b ∈ B.
-inductive B_name : Type (u+1)
+inductive B_name (β : Type u) [complete_boolean_algebra β] : Type (u+1)
 | mk (α : Type u) (A : α → B_name) (B : α → β) : B_name
+
+variables {β : Type u} [complete_boolean_algebra β]
 
 instance nonempty_B_name : nonempty $ @B_name β _ := begin apply nonempty.intro, refine ⟨_,_,_⟩, exact ulift empty, intro x, repeat{cases x}, intro x, repeat{cases x} end
 
@@ -24,15 +66,34 @@ begin refine ⟨_,_,_⟩, exact ulift empty, intro x, repeat{cases x}, intro x, 
   some element of the second family and vice-versa. -/
 def bool_equiv : ∀ (x y : B_name β), β
 /- ∀ x ∃ y, m x y ∧ ∀ y ∃ x, m y x, but this time in ~lattice~ -/
-| (B_name.mk α A B) (B_name.mk α' A' B') := (infi $ (λ a : α, supr $ λ a', bool_equiv (A a) (A' a'))) ⊓ (infi $ λ a', supr $ λ a, bool_equiv (A a) (A' a'))
+| (B_name.mk α A B) (B_name.mk α' A' B') := (infi $ (λ a : α, supr $ λ a', (bool_equiv (A a) (A' a')) ⊓ (B a ⊓ B' a'))) ⊓ (infi $ λ a', supr $ λ a, (bool_equiv (A a) (A' a')) ⊓ (B a ⊓ B' a'))
 
-theorem bool_equiv_refl_empty : (bool_equiv β) (preempty β) (preempty β) = ⊤ :=
+theorem bool_equiv_refl_empty : (@bool_equiv β _) (preempty) (preempty) = ⊤ :=
   by unfold preempty bool_equiv;
   {simp only [lattice.inf_eq_top_iff, lattice.infi_eq_top], fsplit; intros i; cases i; cases i}
 
 open lattice
 
-theorem bool_equiv_refl : ∀(x), bool_equiv β x x = ⊤ :=
-by intro x; induction x; simp[bool_equiv]; split; intros;
-  from (top_unique $ le_supr_of_le i $ by rwa[x_ih i])
+@[simp]theorem bool_equiv_refl : ∀(x), @bool_equiv β _ x x = ⊤ :=
+begin
+  intro x, induction x, simp[bool_equiv], split; intros,
+  apply top_unique, apply le_supr_of_le i, have := x_ih i,
+end
+-- by {intro x; induction x; simp[bool_equiv]; split; intros;
+-- begin end }
+  -- from (top_unique $ le_supr_of_le i $ by {rwa[x_ih i],  })
 
+/-- `x ∈ y` as Boolean-valued pre-sets if `x` is extensionally equivalent to a member
+  of the family `y`. -/
+def mem : B_name β → B_name β → β
+| a (B_name.mk α' A' B') := supr (λ a', @bool_equiv β _ a (A' a'))
+
+infix ` ∈ᴮ `:50 := mem
+
+theorem mem.mk {α : Type*} (A : α → B_name β) (B : α → β) (a : α) : A a ∈ᴮ B_name.mk α A B = ⊤ :=
+by {apply top_unique, apply le_supr_of_le a, simp}
+
+protected def subset : B_name β → B_name β → β
+| (B_name.mk α A B) b := ⨅a:α, A a ∈ᴮ b
+
+end bSet
