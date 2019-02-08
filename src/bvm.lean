78b2d69985e6c64ce3ebff2ec7dd7f63f83d7191
flypitch <jmh288@pitt.edu>
2019-02-07 19:23:33 -0500

    Cleanup in bvm.lean
    State the mixing lemma

--- src/bvm.lean
+++ src/bvm.lean
@@ -1,4 +1,4 @@
-import fol set_theory.zfc order.boolean_algebra order.complete_boolean_algebra
+import fol set_theory.zfc order.boolean_algebra order.complete_boolean_algebra .cohen_poset
 
 open lattice
 
@@ -48,8 +48,10 @@ end lattice
 
 namespace bSet
 
-lemma forall_and {α : Type*} {p q : α → Prop} : (∀ x, p x) ∧ (∀ y, q y) ↔ ∀ x, p x ∧ q x :=
-  by finish
+-- note: use forall_and_distrib in core, which is the same statement but with the ↔ reversed
+
+-- lemma forall_and {α : Type*} {p q : α → Prop} : (∀ x, p x) ∧ (∀ y, q y) ↔ ∀ x, p x ∧ q x :=
+--   by finish
 
 -- τ is a B-name if and only if τ is a set of pairs of the form ⟨σ, b⟩, where σ is
 -- a B-name and b ∈ B.
@@ -60,12 +62,11 @@ export B_name
 
 variables {β : Type u} [complete_boolean_algebra β]
 
-instance nonempty_B_name : nonempty $ @B_name β _ :=
-  by {apply nonempty.intro, refine ⟨_,_,_⟩, exact ulift empty, intro x,
-                          repeat{cases x}, intro x, repeat{cases x}}
-
 def empty : B_name β :=
-  by {fsplit, exact ulift empty, intro x, repeat{cases x}, intro x, repeat{cases x}}
+  ⟨ulift empty, empty.elim ∘ ulift.down, empty.elim ∘ ulift.down⟩
+
+instance nonempty_B_name : nonempty $ @B_name β _ :=
+  ⟨empty⟩
 
 instance has_empty_bSet : has_emptyc (B_name β) := ⟨empty⟩
 
@@ -75,10 +76,11 @@ instance has_empty_bSet : has_emptyc (B_name β) := ⟨empty⟩
 def bool_equiv : ∀ (x y : B_name β), β
 /- ∀ x ∃ y, m x y ∧ ∀ y ∃ x, m y x, but this time in ~lattice~ -/
 | (mk α A B) (mk α' A' B') :=
-             (infi $ (λ a : α, B a ⟹ (supr $ λ a', B' a' ⊓ (bool_equiv (A a) (A' a'))))) ⊓
-               (infi $ (λ a' : α', B' a' ⟹ (supr $ λ a, B a ⊓(bool_equiv (A a) (A' a')))))
+             (⨅a : α, B a ⟹ ⨆a', B' a' ⊓ bool_equiv (A a) (A' a')) ⊓
+               (⨅a' : α', B' a' ⟹ ⨆a, B a ⊓ bool_equiv (A a) (A' a'))
 
-infix ` =ᴮ `:50 := bool_equiv
+
+infix ` =ᴮ `:80 := bool_equiv
 
 theorem bool_equiv_refl_empty : (@bool_equiv β _) (empty) (empty) = ⊤ :=
   by unfold empty bool_equiv;
@@ -86,7 +88,7 @@ theorem bool_equiv_refl_empty : (@bool_equiv β _) (empty) (empty) = ⊤ :=
 
 open lattice
 
-@[simp]theorem bool_equiv_refl : ∀(x), @bool_equiv β _ x x = ⊤ :=
+@[simp]theorem bool_equiv_refl : ∀ x, @bool_equiv β _ x x = ⊤ :=
 begin
   intro x, induction x, simp[bool_equiv, -imp_top_iff_le], split; intros;
   {apply top_unique, simp, apply le_supr_of_le i, have := x_ih i, finish}
@@ -94,19 +96,20 @@ end
 
 /- empty' is the singleton B_name {⟨∅, ⊥⟩}, i.e. a set whose only member is ∅ which has
    a zero probability of actually being an element. It should be equivalent to ∅. -/
-def empty' : B_name β := mk punit (λ x, ∅) (λ x, ⊥)
+def empty' : B_name β := mk punit (λ _, ∅) (λ _, ⊥)
 
-example : ((empty : B_name β) =ᴮ (empty' : B_name β) : β) = ⊤ :=
-  by {simp[empty, empty', bool_equiv], intro i, repeat{cases i}} -- phew
+example : empty =ᴮ empty = (⊤ : β) :=
+  by tidy
 
 /-- `x ∈ y` as Boolean-valued pre-sets if `x` is extensionally equivalent to a member
   of the family `y`. -/
 def mem : B_name β → B_name β → β
-| a (mk α' A' B') := supr (λ a', B' a' ⊓ (a =ᴮ (A' a')))
+| a (mk α' A' B') := ⨆a', B' a' ⊓ a =ᴮ A' a'
 
-def empty'' : B_name β := mk (ulift bool) (λ x, ∅) (λ x, by {repeat{cases x}, exact ⊥, exact ⊤})
+def empty'' : B_name β :=
+  mk (ulift bool) (λ x, ∅) (λ x, by {repeat{cases x}, exact ⊥, exact ⊤})
 
-infix ` ∈ᴮ `:50 := mem
+infix ` ∈ᴮ `:80 := mem
 
 /-- ∅ appears in empty'' with probability 0 and 1, with the higher probability winning the
     vote of membership. This demonstrates why the inequality in the following theorem is
@@ -118,6 +121,11 @@ theorem mem.mk {α : Type*} (A : α → B_name β) (B : α → β) (a : α) : A 
   le_supr_of_le a $ by simp
 
 protected def subset : B_name β → B_name β → β
-| (mk α A B) b := ⨅a:α, A a ∈ᴮ b
+| (mk α A B) b := ⨅a:α, B a ⟹ (A a ∈ᴮ b)
+
+/-- Mixing lemma, c.f. Bell's book or Lemma 1 of Hamkins-Seabold -/
+
+lemma mixing_lemma {A : set β} (h_anti : antichain A) (τ : A → B_name β) :
+  ∃ x, ∀ a : β, a ∈ A → a ≤ x =ᴮ τ ⟨a, by assumption⟩ := sorry
 
 end bSet
