396d7304254d37c47f093291988c12517510681c
flypitch <jmh288@pitt.edu>
2019-05-22 10:15:05 -0400

    Move `independence.lean` to /old
    Remove `independence.lean` from /src

--- old/independence.lean
+++ old/independence.lean
@@ -4,15 +4,11 @@ Released under Apache 2.0 license as described in the file LICENSE.
 
 Authors: Jesse Han, Floris van Doorn
 -/
-import .fol .zfc .completeness
+import .fol .zfc' .completeness
 
 local attribute [instance, priority 0] classical.prop_decidable
 
-open fol
-
-/- Statement of the independence of the continuum hypothesis -/
-
-open zfc
+open fol lattice
 
 section independence
 
@@ -29,24 +25,17 @@ by exact ⟨by {apply unprovable_of_model_negation, exact hT, exact M2.snd, repe
          by {apply unprovable_of_model_negation, exact hT, exact M1.snd, assumption, simpa}⟩
 
 --TODO(everyone)
-theorem ZFC_consistent : is_consistent ZFC :=
-begin
-  apply (model_existence ZFC).mpr, sorry
-end
+theorem CH_consistent {β} [nontrivial_complete_boolean_algebra β] : ∃ M : bModel β ZFC', (nonempty M.fst) ∧ ⊤ ⊩[M.1] CH_f := sorry
 
---TODO(everyone)
-theorem CH_consistent : ∃ M : Model ZFC, (nonempty M.fst) ∧ M ⊨ continuum_hypothesis := sorry
+--TODO(jesse)
+theorem neg_CH_consistent {β} [nontrivial_complete_boolean_algebra β] : ∃ M : bModel β ZFC', (nonempty M.fst) ∧ ⊤ ⊩[M.1] ∼ CH_f := sorry
 
---TODO(everyone)
-theorem neg_CH_consistent : ∃ M : Model ZFC,(nonempty M.fst) ∧ M ⊨ ∼ continuum_hypothesis := sorry
-
-theorem independence_of_CH : (¬ ZFC ⊢' continuum_hypothesis) ∧ (¬ ZFC ⊢' ∼ continuum_hypothesis) :=
-begin
-  have := CH_consistent, have := neg_CH_consistent, repeat{auto_cases},
-  apply @independence_of_exhibit_models L_ZFC ZFC ZFC_consistent continuum_hypothesis,
-  show Model ZFC, exact this_w_1, show Model ZFC, exact this_w, repeat{assumption}
-end
+-- theorem independence_of_CH : (¬ ZFC' ⊢' CH_f) ∧ (¬ ZFC' ⊢' ∼ CH_f) :=
+-- begin
+--   have := CH_consistent, have := neg_CH_consistent, repeat{auto_cases},
+--   apply @independence_of_exhibit_models L_ZFC' ZFC' ZFC'_consistent CH_f,
+--   show Model ZFC', exact this_w_1, show Model ZFC', exact this_w, repeat{assumption}
+-- end
   
-
 
 end independence
