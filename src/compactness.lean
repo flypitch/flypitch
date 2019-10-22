e804a71aabbd219bad4f7b5aa80f0ddb8aebcb05
Jesse Michael Han <hyrodi@gmail.com>
2019-10-21 20:13:38 -0400

    Remove authors

--- src/compactness.lean
+++ src/compactness.lean
@@ -2,7 +2,7 @@
 Copyright (c) 2019 The Flypitch Project. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 
-Authors: Jesse Han, Floris van Doorn
+Authors: 
 -/
 import .fol
 
