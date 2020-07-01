0e91b20a8ce862c45de4f7568dfd65e97669a574
Jesse Michael Han <hyrodi@gmail.com>
2020-06-30 18:09:36 -0700

    new file stub

---
+++ src/pSet_ZFC.lean
@@ -0,0 +1,13 @@
+/-
+Copyright (c) 2020 The Flypitch Project. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author(s): Jesse Michael Han
+
+Showing that pSet is formally a model of ZFC.
+-/
+
+import .zfc'
+import .pSet_ordinal
+import tactic
+
+#check 3
